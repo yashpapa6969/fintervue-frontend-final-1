42d8e404efaa2cec31a088e2eb6d26f632c777bd
unknown <shiveshtiwari0@gmail.com>
2024-08-19 20:53:00 +0530

    temp agora

--- src/components/common/VideoCall.jsx
+++ src/components/common/VideoCall.jsx
@@ -1,95 +1,54 @@
-import { useState } from "react";
+import Cookies from "js-cookie";
+import "../../styles/meeting.css";
+import AgoraVideoCall from "../../components/AgoraVideoCall";
 
-import {
-    LocalUser,
-    RemoteUser,
-    useJoin,
-    useLocalCameraTrack,
-    useLocalMicrophoneTrack,
-    usePublish,
-    useRemoteAudioTracks,
-    useRemoteUsers,
-} from "agora-rtc-react";
-import { Camera, CameraOff, Mic, MicOff, PhoneMissed } from "lucide-react";
-
-
-const VideoCall = ({ micOn, setMic, cameraOn, setCamera }) => {
-
+const Meeting = () => {
+    const videoProfile = Cookies.get("videoProfile")?.split(",")[0] || "480p_4";
+    const channel = Cookies.get("channel") || "test";
+    const transcode = Cookies.get("transcode") || "interop";
+    const attendeeMode = Cookies.get("attendeeMode") || "video";
+    const baseMode = Cookies.get("baseMode") || "avc";
     const appId = import.meta.env.VITE_AGORA_APPID;
 
-    // set the connection state
-    const [activeConnection, setActiveConnection] = useState(true);
+    if (!appId) {
+        alert("Get App ID first!");
+        return null;
+    }
 
-    // get local video and mic tracks
-    const { localMicrophoneTrack } = useLocalMicrophoneTrack(true);
-    const { localCameraTrack } = useLocalCameraTrack(true);
-
-    // Join the channel
-    useJoin(
-        {
-            appid: appId,
-            channel: "test",
-            token: null,
-        },
-        activeConnection,
-    );
-
-    usePublish([localMicrophoneTrack, localCameraTrack]);
-
-    //remote users
-    const remoteUsers = useRemoteUsers();
-    const { audioTracks } = useRemoteAudioTracks(remoteUsers);
-
-    // play the remote user audio tracks
-    audioTracks.forEach((track) => track.play());
-
+    const uid = undefined;
 
     return (
-        <div className="h-screen w-full relative">
-            {activeConnection ? (
-                <>
-                <div id='flex flex-wrap items-center gap-10'>
-                {
-                    // Initialize each remote stream using RemoteUser component
-                    remoteUsers.map((user) => (
-                        <div key={user.uid} className="relative w-1/4 aspect-video overflow-hidden max-w-[720px] min-w-[360px]">
-                            <RemoteUser user={user} />
-                        </div>
-                    ))
-                }
-            </div>
-            <div className="fixed right-10 bottom-10 m-0 w-1/2 aspect-video max-w-[480px] min-w-[360px]">
-                <LocalUser
-                    audioTrack={localMicrophoneTrack}
-                    videoTrack={localCameraTrack}
-                    cameraOn={cameraOn}
-                    micOn={micOn}
-                    playAudio={micOn}
-                    playVideo={cameraOn}
-                />
-            </div>
-            <div className="fixed bottom-0 w-full bg-blue-400 flex items-center justify-center">
-                <div id="flex items-center justify-center">
-                    <button className="btn" onClick={() => setMic(!micOn)}>
-                        {micOn ? <Mic /> : <MicOff />}
-                    </button>
-                    <button className="btn" onClick={() => setCamera(!cameraOn)}>
-                        {cameraOn ? <Camera /> : <CameraOff />}
-                    </button>
-                    <button
-                        onClick={() => {
-                            setActiveConnection(false);
-                        }}
-                    >
-                        <PhoneMissed />
-                    </button>
+        <div className="wrapper meeting">
+            <div className="ag-header">
+                <div className="ag-header-lead">
+                    <img className="header-logo" alt="" />
+                    <span>AgoraWeb v2.1</span>
+                </div>
+                <div className="ag-header-msg">
+                    Room:&nbsp;<span id="room-name">{channel}</span>
                 </div>
             </div>
-            </>) : 
-                <button onClick={() => setActiveConnection(true)}>Rejoin</button>
-            }
+            <div className="ag-main">
+                <div className="ag-container">
+                    <AgoraVideoCall
+                        videoProfile={videoProfile}
+                        channel={channel}
+                        transcode={transcode}
+                        attendeeMode={attendeeMode}
+                        baseMode={baseMode}
+                        appId={appId}
+                        uid={uid}
+                    />
+                </div>
+            </div>
+            <div className="ag-footer">
+                <a className="ag-href" href="https://www.agora.io">
+                    <span>Powered By Agora</span>
+                </a>
+                <span>Talk to Support: 400 632 6626</span>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default VideoCall;
+export default Meeting;
