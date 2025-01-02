95a024c91947e8dbb7f7c0c4bc2c44680baf1a6c
Yash Gupta <108118711+yashpapa6969@users.noreply.github.com>
2025-01-02 23:42:19 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@ const config = {
     development: {
         enableLogging: true,
         uploadBaseUrl:
-            "https://qa2gxw6zd2.execute-api.ap-south-1.amazonaws.com",
+            "hhttps://api.fintervue.com",
         mlBaseUrl: "https://api.fintervue.com",
         apiBaseUrl: "http://localhost:2000",
     },
     production: {
         enableLogging: false,
-        apiBaseUrl: "https://qa2gxw6zd2.execute-api.ap-south-1.amazonaws.com",
+        apiBaseUrl: "https://api.fintervue.com",
         mlBaseUrl: "https://api.fintervue.com",
         uploadBaseUrl: "https://api.fintervue.com",
     },
